d2e189e063299e44913952326623998f40c6787d
loadKK <1780319991@qq.com>
2022-07-02 21:08:00 +0800

    小改动

--- test/t_httpServer.go
+++ test/t_httpServer.go
@@ -8,10 +8,10 @@ import (
 
 // HttpServer 网页访问 localhost:9999 就能看到 Let's Go
 func HttpServer() {
-	f := func(w http.ResponseWriter, r *http.Request) {
+	callback := func(w http.ResponseWriter, r *http.Request) {
 		_, _ = fmt.Fprintf(w, "Let's Go! %s", time.Now())
 	}
 
-	http.HandleFunc("/", f)
-	_ = http.ListenAndServe(":9999", nil) // 监听端口
+	http.HandleFunc("/", callback)
+	_ = http.ListenAndServe(":8888", nil) // 监听端口
 }
